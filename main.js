a1433807ea482f90f32d28ac9a878995dd1c5869
Joaquin Cruz <joacocruz6@gmail.com>
2019-01-06 14:04:32 -0300

    changed the proyect to a simpler one

--- main.js
+++ main.js
@@ -1,78 +1,25 @@
-let regionsName=["forlindon","north ered luin",""];
-let regionsWithTown=[];
-let regionsWithFort=[];
-let regionsWithSiege=[];
-let nations={};
-let regions=[];
-function Region(name,town,fort,siege,nation){
-  this.name=name;
-  this.town=town;
-  this.fort=fort;
-  this.siege=siege;
-  this.nation=nation;
-  this.getName=function(){
-    return this.name;
-  };
-  this.haveTown=function(){
-    return this.town;
-  };
-  this.haveFort=function(){
-    return this.fort;
-  };
-  this.haveSiege=function(){
-    return this.siege;
-  };
-  this.getNation=function(){
-    return this.nation;
-  };
-}
-let regionBuilder={
-  regionName: "",
-  regionTown: false,
-  regionFort: false,
-  regionSiege: false,
-  nation: "",
-  withRegionName: function(name){
-    this.regionName=name;
-    return this;
-  },
-  withTown: function(haveTown){
-    this.regionTown=haveTown;
-    return this;
-  },
-  withFort: function(haveFort){
-    this.regionFort=haveFort;
-    return this;
-  },
-  withSiege: function(haveSiege){
-    this.regionSiege=haveSiege;
-    return this;
-  },
-  withNationName: function(nationName){
-    this.nation=nationName;
-    return this;
-  },
-  build: function(){
-    return new Region(this.regionName,this.regionTown,this.regionFort,this.regionSiege,this.nation);
-  }
-}
-const functions={
-  /**
-  *True if x is in the array
-  *@param x the element to search
-  *@param array the array that is going to be searched
-  *@return True if x in array, False otherwise
-  */
-  contiene: function(x,array){
-    for(let i=0; i<array.lenght;i++){
-      if(array[i].toLowerCase() === x){
-        return true;
-      }
-    }
-    return false;
-  },
-  buscarRegion: function(region,regiones){
-    return;
-  }
+/**
+ * This will be global variables, they will be an object that knows every town with it's nation.
+ * An array that know the spaces with town (that you can create unit but are not forts or sieges)
+ * Another array that have the spaces with forts and the last array that have the sieges
+ */
+let nations = {
+  dwarfs: ["ered luin","north ered luin","erebor","iron hills"],
+  elves: ["grey havens","rivendell","lórien","woodland realm"],
+  theNorth: ["the shire","buckland","north downs","bree","carrock","rhosgobel","old forest road","dale"],
+  gondor: ["anfalas","erech","dol amroth","lamedon","pelargir","lossarnach","minas tirith","druadan forest"],
+  rohan: ["fords of isen","helm's deep","westemnet","edoras","eastemnet","folde"],
+  isengard: ["north dunland","south dunland","gap of rohan","orthanc"],
+  sauron: ["angmar","mount gram","mount gundabad","morannon","barad~dûr","minas morgul","gorgoth","nurn","moria","dol guldur","southern mirkwood"],
+  southronEasterling: ["north rhûn","east rhûn","south rhûn","umbar","near harad","far harad","khand"],
+  neutral: ["west harondor","east harondor","south ithilien","osgiliath","north ithilien","dead marshes","western emyn muil","eastern emyn muil","dagorlad","ash mountains","fangorn"
+            ,"parth celebrant","western brown lands","eastern brown lands","noman~lands","southern dorwinion","dimrill dale","south anduin vale","southern rhovanion","northern dorwinion",
+            "north anduin vale","narrows of the forest","eastern mirkwood","northern rhovanion","vale of celduin","gladden fields","vale of the carnen","high pass","goblin's gate","old ford","western mirkwood"
+            ,"eagles' eyrie","northern mirkwood","withered heath","andrast","drûwaith iaur","enedwaith","minhiriath","cardolan","south ered luin","old forest","south downs","hollin","harlindon"
+            ,"tower hills","weather hills","trollshaws","fords of bruinen","forlindon","evendim","arnor","ettenmoors"] //todo
 };
-module.exports = functions;
+let withTowns = ["orthanc","grey havens","ered luin","the shire","angmar","mount gundabad","rivendell","helm's deep","moria","lórien","carrock","woodland realm"
+                ,"dale","erebor","iron hills","north rhûn","south rhûn","westemnet","edoras","folde","minas tirith","lossarnach","pelargir","dol amrroth","lamedon","umbar"
+                ,"near harad","far harad","nurn","gorgoth","minas morgul","morannon","barad~dûr"];
+let withFort = []; 
+
--- test.js
+++
@@ -1,4 +0,0 @@
-const functions = require('./main');
-test("Simple contine test",()=>{
-  expect(functions.contiene("forlindon",[])).toBeFalsy()
-});
